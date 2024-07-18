8465d0934372cf8b60155a3a1dacdba73bdb1941
xpcosmos <mikeias.d.s.o@gmail.com>
2024-07-18 20:25:35 -0300

    Adding documentation to code

--- test/support_spark/test_start_spark_session.py
+++ test/support_spark/test_start_spark_session.py
@@ -1,4 +1,5 @@
-
+# pylint: disable=unused-wildcard-import, wildcard-import, unused-import, redefined-builtin, broad-exception-caught
+"""Test if Spark Session is working appropriately"""
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
@@ -8,6 +9,7 @@ from pyspark.sql.functions import *
 
 
 class TestSparkSession:
+    """Test Spark Session"""
     __test__ = False
     def __init__(self):
         self.__session = self.__get_spark_session()
@@ -19,6 +21,7 @@ class TestSparkSession:
         return spark
 
     def stop(self):
+        """Stop Spark Session"""
         self.__session.stop()
 
     def __enter__(self) -> SparkSession:
@@ -27,7 +30,7 @@ class TestSparkSession:
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         try:
             self.__session.stop()
-        except:
-            print(self.__get_spark_session())
+        except Exception as error:
+            print(self.__get_spark_session(), error, sep='\n')
         finally:
             print('Session already terminated')
