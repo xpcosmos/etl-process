7ea482658f04bc8f20c9fd8f3834aac20372d855
xpcosmos <mikeias.d.s.o@gmail.com>
2024-07-20 13:44:44 -0300

    Fixing problem with multline

--- spark/read_csv.py
+++ spark/read_csv.py
@@ -4,9 +4,23 @@ spark = SparkSession.builder \
             .appName("ReaderCsv") \
             .getOrCreate()
 
-csv_file_path = '/spark/data/data.csv'
+csv_file_path = 'spark/data/data.csv'
 
-df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
+df = spark.read.option("multiline",True) \
+                .option("encoding", "utf-8")\
+                .csv(csv_file_path, 
+                    header=True, 
+                    sep=',')
 
 print(df.printSchema())
 print(df.show(5))
+
+
+# Saving data to a JDBC source
+df.write \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql:dbserver") \
+    .option("dbtable", "schema.tablename") \
+    .option("user", "username") \
+    .option("password", "password") \
+    .save()
